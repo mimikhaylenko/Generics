b514e0ce1ddbecb1398886a3bc66899465888f3d
yanamikha <yanamikha@gmail.com>
2020-02-13 16:23:23 +0200

    Add files via upload

---
+++ 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using _3;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Remove("23");
+            myDictionary.Add("23", "32");
+            myDictionary.Remove("23");
+       
+        }
+    }
+     static class MyDictionaryExtensions 
+    {
+        public static void Remove<TKey, TValue>(this MyDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            for (int i = 0;i<dictionary.Count;i++)
+            {
+                if (dictionary.Keys[i].Equals(key))
+                {
+                    dictionary.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
